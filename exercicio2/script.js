61003d60125dfbcfbd1dd7b0032f92a95d81cb12
Davi <Daviandreozzi7@gmail.com>
2022-07-27 21:39:23 -0300

    a10-arrays

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,14 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const numeros = [1,2,4,5,6]
+const string = ["lua", "estrelas", "Amor"]
+const sortido = [2, 7, "lua", "cheia", true]
+
+console.log(numeros.length)
+console.log(string.length)
+console.log(sortido.length)
+
+console.log(numeros.includes(4))
+console.log(sortido.includes(5))
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,13 @@
+const numeros = [1,2,4,5,6]
+const string = ["lua", "estrelas", "Amor"]
+const sortido = [2 ,7, "lua", "cheia", true]
+
+const numerosCopia = numeros
+console.log(numeros,numerosCopia.push(3))
+
+const stringCopia = string
+console.log(string,stringCopia.pop())
+
+const sortidoCopia = sortido
+console.log(sortido, sortidoCopia.slice(2))
+
